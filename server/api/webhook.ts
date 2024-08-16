7b794f4c7cd2da10df22bd4d6fb4584543678e4c
balsipson <thirdi@gmail.com>
2024-08-16 21:03:47 +0530

    why

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -1,19 +1,25 @@
-import { isSubscriptionRequest, handleSubscription, processWebhookBody } from '~/server/utils/webhookHelpers'
+import {
+	isSubscriptionRequest,
+	handleSubscription,
+	processWebhookBody,
+} from "~/server/utils/webhookHelpers";
 
 export default defineEventHandler(async (event: any) => {
-  try {
-    const query = getQuery(event);
-    
-    if (Object.keys(query).length > 0 && isSubscriptionRequest(query)) {
-      return handleSubscription(query);
-    }
+	try {
+		const query = getQuery(event);
 
-    // If no query params or not a subscription request, process the message
-    const body = await readBody(event);
-    return await processWebhookBody(body);
+		if (Object.keys(query).length > 0 && isSubscriptionRequest(query)) {
+			return handleSubscription(query);
+		}
 
-  } catch (error) {
-    console.error(error);
-    return { error: "An error occurred processing the request" };
-  }
+		// If no query params or not a subscription request, process the message
+		const body = await readBody(event);
+		const res = await processWebhookBody(body);
+
+		console.log("response: ", res);
+		return await processWebhookBody(body);
+	} catch (error) {
+		console.error(error);
+		return { error: "An error occurred processing the request" };
+	}
 });
