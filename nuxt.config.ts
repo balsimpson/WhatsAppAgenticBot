f54e49cfb1dd7b03a5756a74ae6a181abc40c2d6
balsipson <thirdi@gmail.com>
2024-08-16 16:40:49 +0530

    added vercel edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  nitro: {
+		preset: "vercel-edge",
+		storage: {
+			data: { driver: "vercelKV" },
+		},
+	},
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
 })
